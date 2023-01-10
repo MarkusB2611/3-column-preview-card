f54a2b1da83bf71ae2ffb7ac9a8e121c5e48075c
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-11 00:45:37 +0100

    change color of button text

--- src/components/car-information.tsx
+++ src/components/car-information.tsx
@@ -2,7 +2,7 @@ const CarInformation = ({ car }: CarInformationProps) => {
   return (
     <div
       className={`w-full sm:w-[19.25rem] p-12 first:rounded-t-lg last:rounded-b-lg sm:first:rounded-tr-none sm:first:rounded-l-lg sm:last:rounded-bl-none sm:last:rounded-r-lg`}
-      style={{ background: car.color }}
+      style={{ background: car.color, color: car.color }}
     >
       <div className="mb-9">
         <img src={`/public/images/icon-${car.name}.svg`} alt="" />
@@ -10,8 +10,10 @@ const CarInformation = ({ car }: CarInformationProps) => {
       <h1 className="uppercase font-bigShouldersDisplay text-title mb-6 text-[#F2F2F2]">
         {car.name}
       </h1>
-      <p className="mb-6 sm:mb-[5.25rem]">{car.info}</p>
-      <button className="bg-white text-[#006971] px-8 py-3 rounded-3xl hover:bg-transparent hover:text-white border-2 border-white">
+      <p className="mb-6 sm:mb-[5.25rem] text-white">{car.info}</p>
+      <button
+        className={`bg-white px-8 text-inherit py-3 rounded-3xl hover:bg-transparent hover:text-white border-2 border-white`}
+      >
         Learn More
       </button>
     </div>
